eebd69231107326c23c86da16962394ef586c9ca
SterlingDWatts <SterlingDWatts@gmail.com>
2025-09-04 15:06:31 -0700

    feat: 🎸 update eslint config prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
-import pkg from "eslint-config-prettier/flat";
-
-const { eslintConfigPrettier } = pkg;
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
